3e1b191dab6e586bb75223e8e960bc58451ddaec
folke99 <folkefd@gmail.com>
2023-05-04 23:36:47 +0200

    removed old access token (inactive)

--- commands.py
+++ commands.py
@@ -8,7 +8,7 @@ def activated(recognizer: VoiceRecognizer.VoiceRecognizer):
         if text == "hey spotify" or text == "play spotify" or text == "pay spotify":
             print("RECOGNIZED")
             playsound('activated.wav')
-            spotify = SpotifyApi("BQAbf6mb-i6p1cGV1Ut-sGMYydWN3QfX5hjV07ZyZWoszQCRTqI4OG_Ct0kms_-jlDJ-oslqHPRNW9O2pu8_z8bWtUMmhIYM7BlHX0YmLL1NbIHvbrpwjf8beOM1P28njikrW5WiUHz-5yq9JNRPZgnMGiwN5wNCvCmb_upXNOeTxwdKexL9lW0WsfmjHl1gcmDDOLsHPb9qsbJA3Y8q3dGt2Jhy11KbF423")  # instantiate SpotifyApi class
+            spotify = SpotifyApi("")  # instantiate SpotifyApi class with access token
             setAction(recognizer, spotify)
             # do something here
         else:
